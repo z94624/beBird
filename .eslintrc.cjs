57e9537a1305378eb38f25ebfe3536cb75df698d
smoBEE <z94624@gmail.com>
2024-03-06 00:39:36 +0800

    ci: 設定 ESLint (參考：https://ithelp.ithome.com.tw/articles/10302190)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,43 @@
 module.exports = {
-    root: true,
     env: {
-        node: true,
+        browser: true,
         es2021: true,
+        node: true,
     },
     extends: [
         "eslint:recommended",
-        "@vue/typescript/recommended",
+        "plugin:@typescript-eslint/recommended",
         "plugin:vue/vue3-essential",
-        "@vue/standard",
     ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parser: "vue-eslint-parser",
     parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
         parser: "@typescript-eslint/parser",
-        ecmaVersion: 2020,
     },
+    plugins: ["@typescript-eslint", "vue"],
     rules: {
-        // 這邊可以自訂規則
+        "@typescript-eslint/ban-types": [
+            // 關閉錯誤(error)：不要以 {} 當作一個類型
+            "error",
+            {
+                extendDefaults: true,
+                types: {
+                    "{}": false,
+                },
+            },
+        ],
+        "@typescript-eslint/no-explicit-any": ["off"], // 關閉警告(warning)：不允許使用 any
     },
 };
