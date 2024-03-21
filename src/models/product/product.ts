ed852224110c30969d8bd2e45d78d698ece5c5c1
smoBEE <z94624@gmail.com>
2024-03-22 00:08:26 +0800

    types: 補 product API Response

--- src/models/product/product.ts
+++ src/models/product/product.ts
@@ -27,3 +27,48 @@ export class PRODUCTGetChecklistFeedOnDateReq {
     sortKey?: SortKeyEnum = SortKeyEnum.DATE_OF_CHECKLIST; // Order the results by the date of the checklist or by the date it was submitted.
     maxResults?: number = 10; // Only fetch this number of checklists.
 }
+
+/**
+ * View Checklist
+ */
+export interface IPRODUCTGetViewChecklistRes {
+    projId: string;
+    subId: string;
+    protocolId: string;
+    locId: string;
+    durationHrs: number;
+    allObsReported: boolean;
+    creationDt: string; // YYYY-MM-DD HH:mm
+    lastEditedDt: string; // YYYY-MM-DD HH:mm
+    obsDt: string; // YYYY-MM-DD HH:mm
+    obsTimeValid: boolean;
+    checklistId: string;
+    numObservers: number;
+    subnational1Code: string;
+    submissionMethodCode: string;
+    userDisplayName: string;
+    obs: IPRODUCTGetViewChecklistItem[];
+}
+export interface IPRODUCTGetViewChecklistItem {
+    speciesCode: string;
+    hideFlags: [];
+    obsDt: string; // YYYY-MM-DD HH:mm
+    subnational1Code: string;
+    howManyAtleast: number;
+    howManyAtmost: number;
+    howManyStr: string;
+    present: boolean;
+    projId: string;
+    subId: string;
+    obsId: string;
+    obsAux?: IPRODUCTGetViewChecklistAux[];
+}
+export interface IPRODUCTGetViewChecklistAux {
+    subId: string;
+    obsId: string;
+    speciesCode: string;
+    fieldName: string;
+    entryMethodCode: string;
+    auxCode: string;
+    value: string;
+}
