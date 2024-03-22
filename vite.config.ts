e2057e7d48d2c8c9076712771685b0137792d7f6
smoBEE <z94624@gmail.com>
2024-03-22 21:38:08 +0800

    ci: 修正 Quasar 樣式複寫檔設定

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
         // @quasar/plugin-vite options list:
         // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
         quasar({
-            sassVariables: 'src/quasar-variables.sass',
+            sassVariables: 'src/styles/quasar-variables.scss',
         }),
         AutoImport({
             // targets to transform
