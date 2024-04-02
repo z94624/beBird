0712d36a2b9fb8d481c74de298875f7def573a49
smoBEE <z94624@gmail.com>
2024-04-02 22:54:28 +0800

    ci: 初始化 Pinia Store

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,13 @@ import '@quasar/extras/material-icons/material-icons.css';
 // Import Quasar css
 import 'quasar/src/css/index.sass';
 
+import { setupStore } from './store';
+
 async function bootstrap() {
     const app = createApp(App);
+
+    // 掛載狀態管理
+    setupStore(app);
 
     app.use(Quasar, {
         plugins: {}, // import Quasar plugins and add here
