7eb9a186bba4036b465f6423699840958c0ed941
smoBEE <z94624@gmail.com>
2024-04-17 22:19:04 +0800

    feat: Layout 手機版完成

--- src/layout/Layout-M.vue
+++ src/layout/Layout-M.vue
@@ -1,9 +1,83 @@
 <template>
     <div>
+        <q-layout view="hHh lpR fFf">
 
+            <q-header reveal elevated class="bg-white text-primary_e">
+                <q-toolbar>
+                    <div @click="goHome">
+                        <EbirdLogo width="100%" height="26" />
+                    </div>
+
+                    <q-toolbar-title></q-toolbar-title>
+
+                    <q-btn icon="menu" flat round @click="toggleLeftDrawer" />
+                </q-toolbar>
+            </q-header>
+
+            <q-drawer v-model="leftDrawerOpen" side="right" behavior="mobile" :width="200" overlay bordered
+                class="text-primary_e">
+                <q-scroll-area class="fit">
+                    <q-list class="layoutMenuList">
+
+                        <template v-for="(menuItem, mIdx) in menuList" :key="mIdx">
+                            <q-item :to="menuItem.to" :active="menuItem.name === selectedMenu" clickable
+                                :class="[`${menuItem.name === 'rareBirds' ? 'rareBirdsItem' : ''}`]"
+                                @click="() => onSelectMenu(menuItem.name)">
+                                <q-item-section avatar>
+                                    <q-icon :name="menuItem.icon" />
+                                </q-item-section>
+                                <q-item-section>
+                                    {{ menuItem.label }}
+                                </q-item-section>
+                            </q-item>
+                            <q-separator :key="'sep' + mIdx" v-if="menuItem.separator" />
+                        </template>
+
+                    </q-list>
+                </q-scroll-area>
+            </q-drawer>
+
+            <q-page-container style="height: 100vh">
+                <router-view />
+            </q-page-container>
+
+        </q-layout>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import EbirdLogo from '@/assets/images/components/EbirdLogo.vue';
+
+import { PageEnum } from '@/models/enum/pageEnum';
+import { menuList } from './utils';
+
+const router = useRouter();
+
+const leftDrawerOpen = ref(false);
+const selectedMenu = ref('rareBirds');
+
+/**
+ * 開關選單
+ */
+const toggleLeftDrawer = () => {
+    leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+/**
+ * 導向首頁
+ */
+const goHome = () => {
+    router.replace(PageEnum.BASE_HOME);
+}
+
+/**
+ * 選擇選單
+ */
+const onSelectMenu = (menuName: string) => {
+    selectedMenu.value = menuName;
+}
+</script>
 
 <style lang="scss" scoped></style>
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -9,11 +9,9 @@
                     </q-toolbar-title>
 
                     <q-tabs align="left" inline-label class="layoutHeaderTabs">
-                        <q-route-tab to="/rare-birds-map" label="稀有鳥地圖" name="rareBirds" class="rareBirdsTab" />
-                        <q-route-tab to="/bird-watching-map" label="鳥人賞鳥地圖" name="birdWatching" />
-                        <q-route-tab to="/bird-distribution-map" label="鳥類分布地圖" name="birdDistribution" />
-                        <q-route-tab to="/bird-composition-map" label="地區組成地圖" name="birdComposition" />
-                        <q-route-tab to="/routine-route-map" label="例行路線地圖" name="routineRoute" />
+                        <q-route-tab v-for="(menuItem, mIdx) in menuList" :key="mIdx" :to="menuItem.to"
+                            :label="menuItem.label" :name="menuItem.name"
+                            :class="[`${menuItem.name === 'rareBirds' ? 'rareBirdsTab' : ''}`]" />
                     </q-tabs>
                 </q-toolbar>
             </q-header>
@@ -31,6 +29,7 @@ import { useRouter } from 'vue-router';
 import EbirdLogo from '@/assets/images/components/EbirdLogo.vue';
 
 import { PageEnum } from '@/models/enum/pageEnum';
+import { menuList } from './utils';
 
 const router = useRouter();
 
