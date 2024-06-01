113b83292d96818a1daa6aef9d78fbbad909678a
smoBEE <z94624@gmail.com>
2024-06-01 15:10:44 +0800

    refactor: VueLeaflet 公用元件 新增綁定 attrs & slots

--- src/components/common/Leaflet/BasicTypes/VlIcon.vue
+++ src/components/common/Leaflet/BasicTypes/VlIcon.vue
@@ -1,14 +1,27 @@
 <template>
 	<l-icon
+		:="attrs"
 		:icon-anchor="[7, 35]"
 		:icon-size="[14, 35]"
 		:icon-url="TWT"
-	/>
+	>
+		<template
+			#[slot]
+			v-for="(slot, sIdx) in Object.keys(slots)"
+			:key="sIdx"
+		>
+			<slot :name="slot"></slot>
+		</template>
+	</l-icon>
 </template>
 
 <script lang="ts" setup>
+	import { useAttrs, useSlots } from 'vue';
 	import { LIcon } from '@vue-leaflet/vue-leaflet';
 	import TWT from '@/assets/images/twt.svg';
+
+	const attrs = useAttrs();
+	const slots = useSlots();
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/common/Leaflet/UILayers/VlTooltip.vue
+++ src/components/common/Leaflet/UILayers/VlTooltip.vue
@@ -1,20 +1,31 @@
 <template>
 	<l-tooltip
+		:="attrs"
 		:options="{
 			offset: [0, -17],
 			permanent,
 		}"
 	>
-		<slot name="default"></slot>
+		<template
+			#[slot]
+			v-for="(slot, sIdx) in Object.keys(slots)"
+			:key="sIdx"
+		>
+			<slot :name="slot"></slot>
+		</template>
 	</l-tooltip>
 </template>
 
 <script lang="ts" setup>
+	import { useAttrs, useSlots } from 'vue';
 	import { LTooltip } from '@vue-leaflet/vue-leaflet';
 
 	const props = defineProps<{
 		permanent?: boolean;
 	}>();
+
+	const attrs = useAttrs();
+	const slots = useSlots();
 </script>
 
 <style lang="scss" scoped></style>
