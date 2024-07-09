a2d0a4479111e014fefd446f8c142d443ba6b092
smoBEE <z94624@gmail.com>
2024-07-09 23:38:04 +0800

    refactor: 暫時只開放「稀有鳥地圖」

--- src/layout/utils.ts
+++ src/layout/utils.ts
@@ -11,28 +11,28 @@ export const menuList: {
 		icon: 'o_campaign',
 		to: '/rare-birds-map',
 	},
-	{
-		name: 'birdWatching',
-		label: '鳥人賞鳥地圖',
-		icon: 'o_group',
-		to: '/bird-watching-map',
-	},
-	{
-		name: 'birdDistribution',
-		label: '鳥類分布地圖',
-		icon: 'o_flutter_dash',
-		to: '/bird-distribution-map',
-	},
-	{
-		name: 'birdComposition',
-		label: '地區組成地圖',
-		icon: 'o_location_on',
-		to: '/bird-composition-map',
-	},
-	{
-		name: 'routineRoute',
-		label: '例行路線地圖',
-		icon: 'o_route',
-		to: '/routine-route-map',
-	},
+	// {
+	// 	name: 'birdWatching',
+	// 	label: '鳥人賞鳥地圖',
+	// 	icon: 'o_group',
+	// 	to: '/bird-watching-map',
+	// },
+	// {
+	// 	name: 'birdDistribution',
+	// 	label: '鳥類分布地圖',
+	// 	icon: 'o_flutter_dash',
+	// 	to: '/bird-distribution-map',
+	// },
+	// {
+	// 	name: 'birdComposition',
+	// 	label: '地區組成地圖',
+	// 	icon: 'o_location_on',
+	// 	to: '/bird-composition-map',
+	// },
+	// {
+	// 	name: 'routineRoute',
+	// 	label: '例行路線地圖',
+	// 	icon: 'o_route',
+	// 	to: '/routine-route-map',
+	// },
 ];
