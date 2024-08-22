7f47f5a6eb8cb4658a3a79f57adeb0d839ad59f5
smoBEE <z94624@gmail.com>
2024-08-22 23:47:24 +0800

    wip: 拜訪人次 決定使用 vue-number-animation 套件

--- src/layout/components/VisitorsBillboard.vue
+++ src/layout/components/VisitorsBillboard.vue
@@ -1,9 +1,52 @@
 <template>
-	<div></div>
+	<div class="flex flex-col gap-1 w-full">
+		<div class="flex p-1">
+			<q-item-section>Online</q-item-section>
+			<q-item-section>
+				<NumberAnimation
+					:duration="3"
+					:format="(val: number) => val.toFixed()"
+					:from="0"
+					:to="online"
+					autoplay
+					easing="easeOutCubic"
+				/>
+			</q-item-section>
+		</div>
+
+		<div class="flex p-1">
+			<q-item-section>Today</q-item-section>
+			<q-item-section>
+				<NumberAnimation
+					:duration="3"
+					:format="(val: number) => val.toFixed()"
+					:from="0"
+					:to="today"
+					autoplay
+					easing="easeOutCubic"
+				/>
+			</q-item-section>
+		</div>
+
+		<div class="flex p-1">
+			<q-item-section>Total</q-item-section>
+			<q-item-section>
+				<NumberAnimation
+					:duration="3"
+					:format="(val: number) => val.toFixed()"
+					:from="0"
+					:to="total"
+					autoplay
+					easing="easeOutCubic"
+				/>
+			</q-item-section>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
 	import { toRefs } from 'vue';
+	import NumberAnimation from 'vue-number-animation';
 
 	import { useVisitorStatStore } from '@/store/modules/firebase';
 
