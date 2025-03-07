fbed7e8761069f4995a82b4210a883402073a685
smoBEE <z94624@gmail.com>
2025-03-07 16:59:48 +0800

    wip: 天氣 > 天氣跳窗 準備開發【天氣面板】...

--- src/layout/components/weather/WeatherDialog.vue
+++ src/layout/components/weather/WeatherDialog.vue
@@ -3,16 +3,53 @@
 		v-model="isOpen"
 		@close="close"
 	>
-		<template #title></template>
+		<template #title>天氣面板</template>
 
-		<template #content></template>
+		<template #content>
+			<q-splitter v-model="split">
+				<template v-slot:before>
+					<BaseTabs
+						v-model="tab"
+						align="left"
+						dense
+						vertical
+					>
+						<q-tab name="tomorrow">
+							<TomorrowIcon
+								height="28"
+								only-icon
+							/>
+						</q-tab>
+					</BaseTabs>
+				</template>
+
+				<template v-slot:after>
+					<q-tab-panels
+						v-model="tab"
+						animated
+						keep-alive
+					>
+						<q-tab-panel name="tomorrow">
+							<WeatherPanel />
+						</q-tab-panel>
+					</q-tab-panels>
+				</template>
+			</q-splitter>
+		</template>
 	</BaseDialog>
 </template>
 
 <script lang="ts" setup>
+	import { ref } from 'vue';
+	import TomorrowIcon from '@/assets/icons/weather/providers/TomorrowIcon.vue';
+	import WeatherPanel from './WeatherPanel.vue';
+
 	import { useDialog } from '@/hooks/dialog';
 
 	const { isOpen, open, onOpen, close, onClose } = useDialog();
+
+	const split = ref(15);
+	const tab = ref('tomorrow');
 
 	onOpen(() => {});
 
