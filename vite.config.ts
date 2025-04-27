a335564324cf6836834f4939c2d66751a891a7ec
smoBEE <z94624@gmail.com>
2025-04-27 13:18:33 +0800

    chore: src 不行，build 階段就會發生錯誤

--- vite.config.ts
+++ vite.config.ts
@@ -126,7 +126,7 @@ export default defineConfig({
 		// @quasar/plugin-vite options list:
 		// https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
 		quasar({
-			sassVariables: 'src/styles/quasar-variables.scss',
+			sassVariables: '@/styles/quasar-variables.scss',
 		}),
 		AutoImport({
 			// targets to transform
