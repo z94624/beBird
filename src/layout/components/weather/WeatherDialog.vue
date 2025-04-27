ec51c08807f6457f5dbb27fc0384722f51c5c9b0
smoBEE <z94624@gmail.com>
2025-04-27 12:08:30 +0800

    refactor: 天氣 > 天氣面板 1. 電腦版非滿版跳窗 2. 手機版頁籤為上下型

--- src/layout/components/weather/WeatherDialog.vue
+++ src/layout/components/weather/WeatherDialog.vue
@@ -1,44 +1,73 @@
 <template>
 	<BaseDialog
 		v-model="isOpen"
-		full-width
+		:full-width="isMobile"
 		@close="close"
 	>
 		<template #title>天氣面板</template>
 
 		<template #content>
-			<q-splitter
-				v-model="split"
-				:limits="[15, 15]"
-			>
-				<template v-slot:before>
-					<BaseTabs
-						v-model="tab"
-						align="left"
-						dense
-						vertical
-					>
-						<q-tab name="tomorrow">
-							<TomorrowIcon
-								height="28"
-								only-icon
-							/>
-						</q-tab>
-					</BaseTabs>
-				</template>
+			<!-- 手機版 -->
+			<template v-if="isMobile">
+				<BaseTabs
+					v-model="tab"
+					align="left"
+					dense
+				>
+					<q-tab name="tomorrow">
+						<TomorrowIcon
+							height="28"
+							only-icon
+						/>
+					</q-tab>
+				</BaseTabs>
 
-				<template v-slot:after>
-					<q-tab-panels
-						v-model="tab"
-						animated
-						keep-alive
-					>
-						<q-tab-panel name="tomorrow">
-							<WeatherPanel :weather-data="weatherData" />
-						</q-tab-panel>
-					</q-tab-panels>
-				</template>
-			</q-splitter>
+				<q-tab-panels
+					v-model="tab"
+					animated
+					keep-alive
+				>
+					<q-tab-panel name="tomorrow">
+						<WeatherPanel :weather-data="weatherData" />
+					</q-tab-panel>
+				</q-tab-panels>
+			</template>
+
+			<!-- 電腦版 -->
+			<template v-else>
+				<q-splitter
+					v-model="split"
+					:limits="[15, 15]"
+				>
+					<template v-slot:before>
+						<BaseTabs
+							v-model="tab"
+							align="left"
+							dense
+							vertical
+						>
+							<q-tab name="tomorrow">
+								<TomorrowIcon
+									height="28"
+									only-icon
+								/>
+							</q-tab>
+						</BaseTabs>
+					</template>
+
+					<template v-slot:after>
+						<q-tab-panels
+							v-model="tab"
+							animated
+							keep-alive
+						>
+							<q-tab-panel name="tomorrow">
+								<WeatherPanel :weather-data="weatherData" />
+							</q-tab-panel>
+						</q-tab-panels>
+					</template>
+				</q-splitter>
+			</template>
 		</template>
 	</BaseDialog>
 </template>
@@ -49,9 +78,11 @@
 	import WeatherPanel from './WeatherPanel.vue';
 
 	import { useDialog } from '@/hooks/dialog';
+	import { usePlatform } from '@/hooks/platform';
 	import { WeatherPanelInfo } from './types';
 
 	const { isOpen, open, onOpen, close, onClose } = useDialog();
+	const { isMobile } = usePlatform();
 
 	const split = ref(15);
 	const tab = ref('tomorrow');
