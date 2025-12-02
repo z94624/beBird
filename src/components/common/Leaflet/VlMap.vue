b96202910b865b0bd53373a5dadcc5fec67bdb95
smoBEE <z94624@gmail.com>
2025-12-02 22:20:58 +0800

    wip: 亮暗模式 > 地圖 > 搜尋鈕 先調整為白底黑字、黑底白字切換

--- src/components/common/Leaflet/VlMap.vue
+++ src/components/common/Leaflet/VlMap.vue
@@ -4,8 +4,9 @@
 		<!-- 搜尋功能 -->
 		<BaseButton
 			v-morph:btn.resize="birdMorph"
+			:color="bg_name_mode"
+			:text-color="text_name_mode"
 			class="map-top-right"
-			color="primary"
 			fab
 			icon="search"
 			round
@@ -18,12 +19,12 @@
 			class="searchMenuContainer w-[33%] shadow-3 rounded-borders relative"
 		>
 			<BaseButton
+				:color="bg_name_mode"
+				:text-color="text_name_mode"
 				class="absolute -left-2 -bottom-2"
-				color="white"
 				icon="arrow_outward"
 				round
 				size="sm"
-				text-color="primary"
 				@click="nextBirdMorph"
 			/>
 			<slot name="search-menu"></slot>
@@ -41,6 +42,7 @@
 		</q-drawer>
 		<!-- 資料更新按鈕 -->
 		<BaseButton
+			:color="bg_name_mode"
 			:icon="fasRotateRight"
 			:loading="updateLoading"
 			:style="{
@@ -49,12 +51,11 @@
 						? `calc(${boundaryGap} + 3.6rem + 0.25rem)`
 						: `calc(${boundaryGap} + ${isTextSizeMd ? '15.75rem' : isTextSizeLg ? '19.1875rem' : '23.35rem'} + 0.25rem)`,
 			}"
+			:text-color="text_name_mode"
 			class="researchBtn"
-			color="secondary"
 			fab
 			round
 			size="lg"
-			text-color="primary"
 			@click="emit('research')"
 		/>
 
@@ -407,8 +408,6 @@
 	}
 	.searchMenuContainer {
 		@extend .map-top-right;
-
-		background-color: rgba($color: #fff, $alpha: 0.75);
 	}
 	.researchBtn {
 		@extend .map-top-right;
