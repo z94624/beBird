42470f6638c146a2f5e4e1f59fa8fcba1c1119b5
smoBEE <z94624@gmail.com>
2025-12-05 21:54:43 +0800

    style: 地圖 > 圖釘 > 彈窗 > 地點 調整斷行設定，避免跑版

--- src/components/common/Leaflet/UILayers/VlPopup.vue
+++ src/components/common/Leaflet/UILayers/VlPopup.vue
@@ -8,7 +8,7 @@
 				class="popupCard flex flex-col gap-2"
 				flat
 			>
-				<q-card-section class="w-full !px-2 !pt-2 !pb-0">
+				<q-card-section class="!px-2 !pt-2 !pb-0">
 					<div class="ellipsis-2-lines break-all">
 						<slot name="location"></slot>
 					</div>
