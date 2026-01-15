498d8bc9c5f5e9042b5fccdb158ce84842f4bfe0
smoBEE <z94624@gmail.com>
2026-01-15 22:04:13 +0800

    style: 版本 佈版訊息

--- src/layout/utils.ts
+++ src/layout/utils.ts
@@ -54,6 +54,13 @@ export const versionList: {
 	perfs?: string[]; // 優化；perf
 }[] = [
 	{
+		version: '1.1.8',
+		releaseDate: '2026/01/15',
+		features: ['Added support for dark/light mode toggle.'],
+		changes: ['All the weather GIFs are replaced with static PNG icons.'],
+		perfs: ['Layout of the location shown in the detail dialog of rare-bird spot is adjusted.'],
+	},
+	{
 		version: '1.1.7',
 		releaseDate: '2025/08/23',
 		features: [
