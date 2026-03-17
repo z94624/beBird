111f5381816853d1ce4329596bed4957752ccf6f
smoBEE <z94624@gmail.com>
2026-03-17 23:15:34 +0800

    wip: 資源來源 新增「引言」區塊

--- src/views/Attributions.vue
+++ src/views/Attributions.vue
@@ -1,5 +1,14 @@
 <template>
-	<div></div>
+	<q-page padding>
+		<div class="page-content-container flex flex-col items-center gap-4">
+			<!-- Quote -->
+			<div class="p-4">
+				<blockquote>
+					<p>beBird 的誕生，站在許多開源巨人的肩膀上。感謝以下優質資源與社群的貢獻。</p>
+				</blockquote>
+			</div>
+		</div>
+	</q-page>
 </template>
 
 <script lang="ts" setup></script>
