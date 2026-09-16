0ba0618ee257479a6db1275e59d2cc0a03ee9efa
smoBEE <z94624@gmail.com>
2026-09-16 21:10:38 +0800

    style: 地圖 > 圖釘 - 捨棄原始的台灣紫嘯鶇圖釘

--- src/components/common/Leaflet/BasicTypes/VlIcon.vue
+++ src/components/common/Leaflet/BasicTypes/VlIcon.vue
@@ -1,31 +1,22 @@
 <template>
 	<l-icon
 		:="attrs"
-		:class-name="hasDefaultSlot ? 'bg-transparent border-none' : ''"
 		:icon-anchor="[14, 32]"
 		:icon-size="[28, 28]"
-		:icon-url="hasDefaultSlot ? undefined : TWT"
+		class-name="bg-transparent border-none"
 	>
-		<template
-			#[slot]
-			v-for="(slot, sIdx) in Object.keys(slots)"
-			:key="sIdx"
-		>
-			<slot :name="slot"></slot>
-		</template>
+		<slot>
+			<GoogleMarkerIcon bg-color="primary" />
+		</slot>
 	</l-icon>
 </template>
 
 <script lang="ts" setup>
-	import { computed, useAttrs, useSlots } from 'vue';
+	import { useAttrs, useSlots } from 'vue';
 	import { LIcon } from '@vue-leaflet/vue-leaflet';
-	import TWT from '@/assets/images/twt.svg';
 
 	const attrs = useAttrs();
 	const slots = useSlots();
-
-	// 判斷父層是否有傳入 default slot
-	const hasDefaultSlot = computed(() => !!slots.default);
 </script>
 
 <style lang="scss" scoped></style>
